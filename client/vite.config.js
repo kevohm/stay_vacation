2f64ea29b85a7533911173b47152352737a29c18
kevohm <kevinkbet@gmail.com>
2025-11-05 08:56:23 +0300

    fix: build folder name

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
+  plugins: [react(), tailwindcss()],
+  build: {
+    outDir: "build", // 👈 this sets the output directory
+  },
 });
